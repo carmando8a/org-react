1a6e62517924b00dd918b06422d5abb61c07460e
carmando8a <88053915+carmando8a@users.noreply.github.com>
2023-10-09 15:01:11 -0600

    first commit

--- src/componentes/Footer/index.jsx
+++ src/componentes/Footer/index.jsx
@@ -4,16 +4,16 @@ const Footer = () => {
     return <footer className="footer" style={{backgroundImage:"url(/img/Footer.png)"}}>
     <div className="redes">
     <a href='https://www.facebook.com/carlos.ochoalupercio/' target="_blank"> 
-        <img src="/img/facebook.png" alt="Facebook"/>
+        <img src="./img/facebook.png" alt="Facebook"/>
     </a>
     <a href="https://twitter.com/Carlos8a" target="_blank"> 
-        <img src="/img/twitter.png" alt="twitter" />
+        <img src="./img/twitter.png" alt="twitter" />
     </a>
     <a href="https://www.instagram.com/charles8ax/" target="_blank"> 
-        <img src="/img/instagram.png" alt="instagram" />
+        <img src="./img/instagram.png" alt="instagram" />
     </a>
     </div>
-    <img src='/img/Logo.png' alt='org' /> 
+    <img src='./img/Logo.png' alt='org' /> 
     <strong>Desarrollado por Carlos 8a</strong>
 </footer>
 }
